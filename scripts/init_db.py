4d7af39c14b098d56d23db7e3c43ed725f5182e0
Aitor <127235588+aitorGonzalo@users.noreply.github.com>
2024-11-24 14:06:29 +0100

    Update init_db.py

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,39 +1,63 @@
 import mysql.connector
 import time
-
-# Espera unos segundos para asegurarse de que MySQL está en funcionamiento
 time.sleep(10)
+# Función para comprobar si MySQL está disponible
+def wait_for_mysql():
+    max_retries = 10
+    delay = 10
+    retries = 0
 
-try:
-   
-    conn = mysql.connector.connect(
-        host="db",
-        user="root",
-        password="1234",
-        database="movie_db",
-        allow_local_infile=True
-    )
+    while retries < max_retries:
+        try:
+            conn = mysql.connector.connect(
+                host="db",
+                user="root",
+                password="1234",
+                database="movie_db",
+            )
+            conn.close()
+            print("MySQL está disponible.")
+            return True
+        except mysql.connector.Error as err:
+            print(f"Esperando a MySQL... (Intento {retries + 1}/{max_retries})")
+            time.sleep(delay)
+            retries += 1
 
-    cursor = conn.cursor()
+    print("MySQL no está disponible después de varios intentos.")
+    return False
 
-    
-    with open('/var/lib/mysql-files/db_init.sql', 'r') as f:
-        sql_statements = f.read().split(';')  
+# Esperar a que MySQL esté disponible
+if wait_for_mysql():
+    try:
+        conn = mysql.connector.connect(
+            host="db",
+            user="root",
+            password="1234",
+            database="movie_db",
+            allow_local_infile=True
+        )
 
-    for statement in sql_statements:
-        statement = statement.strip()
-        if statement:  # Ejecuta solo si la sentencia no está vacía
-            cursor.execute(statement)
-            print(f"Ejecutado: {statement}")
+        cursor = conn.cursor()
 
-    conn.commit()
-    print("Base de datos inicializada correctamente.")
+        with open('/var/lib/mysql-files/db_init.sql', 'r') as f:
+            sql_statements = f.read().split(';')  
 
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
+        for statement in sql_statements:
+            statement = statement.strip()
+            if statement:  
+                cursor.execute(statement)
+                print(f"Ejecutado: {statement}")
 
-finally:
-    if 'cursor' in locals() and cursor:
-        cursor.close()
-    if 'conn' in locals() and conn:
-        conn.close()
+        conn.commit()
+        print("Base de datos inicializada correctamente.")
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+
+    finally:
+        if 'cursor' in locals() and cursor:
+            cursor.close()
+        if 'conn' in locals() and conn:
+            conn.close()
+else:
+    print("No se pudo conectar a MySQL. Saliendo...")
